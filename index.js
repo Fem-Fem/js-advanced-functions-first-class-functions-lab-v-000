9b038b4f34b8869ba4c2cdfd1c026e3d925afdf6
Obafemi Fadairo <ofpupp@princeton.edu>
2018-06-19 23:41:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const returnFirstTwoDrivers = function (array) {
 }
 
 const returnLastTwoDrivers = function (array) {
-  return [array[array.length], array[array.size-1]]
+  return [array[array.length-1], array[array.length-2]]
 }
