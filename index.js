48d4357f274234f691cae94a93abd818c4a07ae8
Obafemi Fadairo <ofpupp@princeton.edu>
2018-06-19 23:54:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ const fareDoubler = createFareMultiplier(2)
 const fareTripler = createFareMultiplier(3)
 
 const selectDifferentDrivers = function (array, thisFunction) {
-  thisFunction(array)
+  return thisFunction(array)
 }
